eb92776576abe89b61ac241d7ea28906c38fecee
bflakke <141943732+bflakke@users.noreply.github.com>
2023-08-10 11:59:29 -0400

    Update streamlit_app.py
    Added Images

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Spinach Omelette - $8.50')
-streamlit.text('Fruit Parfait - $6.75')
-streamlit.text('Kale, Strawberry, Banana Smoothie - $4.95')
+streamlit.text('🥗 Spinach Omelette - $8.50')
+streamlit.text('🥣 Fruit Parfait - $6.75')
+streamlit.text('🥑 Kale, Avocado, Strawberry, and Banana Smoothie - $4.95')
+streamlit.text('🐔 Toasted Chicken and Cream Cheese Bagel - $8.75') 
+
 
