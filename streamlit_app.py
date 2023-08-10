81fc80fde717c52d7bef5c6c4ec9cd95873108af
bflakke <141943732+bflakke@users.noreply.github.com>
2023-08-10 11:54:59 -0400

    Update streamlit_app.py
    add more verbiage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Spinach Omelette - $8.50')
+streamlit.text('Fruit Parfait - $6.75')
+streamlit.text('Kale, Strawberry, Banana Smoothie - $4.95)
+
